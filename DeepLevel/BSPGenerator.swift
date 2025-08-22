74a419b2672240ff6086a9ed3c3034963722dd55
Jeff Kunzelman <jeff@river.io>
2025-08-22 06:14:04 -0500

    fix rebase

--- DeepLevel/BSPGenerator.swift
+++ DeepLevel/BSPGenerator.swift
@@ -194,7 +194,6 @@ final class BSPGenerator: DungeonGenerating {
         let start = rooms.first?.center ?? (config.width/2, config.height/2)
         placeHidingAreas(config: config, rng: &rng, tiles: &tiles, rooms: rooms)
 
-
         return DungeonMap(width: config.width,
                           height: config.height,
                           tiles: tiles,
